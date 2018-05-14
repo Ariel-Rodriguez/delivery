5d47761671b10f3ea9b55ff61f0e0d58293926d7
Ariel Rodriguez <ariel.fdr@gmail.com>
2018-05-14 18:44:02 -0300

    add styled-components.

--- Components/Main.js
+++ Components/Main.js
@@ -1,40 +1,44 @@
+import React from 'react'
+import { injectGlobal } from 'styled-components'
+
+const globals = injectGlobal`
+  * {
+    font-family: "Bitstream Vera Sans Mono", "Courier New", monospace, serif;
+    box-sizing: border-box;
+  }
+  body {
+    margin: 0;
+    padding: 0;
+  }
+  a {
+    text-decoration: none;
+  }
+  p {
+    font-size: 14px;
+    line-height: 24px;
+  }
+  h1 {
+    font-size: 1.5rem;
+  }
+  button {
+    align-items: center;
+    background-color: #22BAD9;
+    border: 0;
+    color: white;
+    display: flex;
+    padding: 5px 7px;
+  }
+  button:active {
+    background-color: #1B9DB7;
+    transition: background-color .3s
+  }
+  button:focus {
+    outline: none;
+  }
+`
+
 export default ({ children }) => (
   <main>
     {children}
-    <style jsx global>{`
-      * {
-        font-family: "Bitstream Vera Sans Mono", "Courier New", monospace, serif;
-        box-sizing: border-box;
-      }
-      body {
-        margin: 0;
-        padding: 0;
-      }
-      a {
-        text-decoration: none;
-      }
-      p {
-        font-size: 14px;
-        line-height: 24px;
-      }
-      h1 {
-        font-size: 1.5rem;
-      }
-      button {
-        align-items: center;
-        background-color: #22BAD9;
-        border: 0;
-        color: white;
-        display: flex;
-        padding: 5px 7px;
-      }
-      button:active {
-        background-color: #1B9DB7;
-        transition: background-color .3s
-      }
-      button:focus {
-        outline: none;
-      }
-    `}</style>
   </main>
 )
